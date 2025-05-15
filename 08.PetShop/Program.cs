166f6e78cfbf3b18d82dc3013c13964a3b307bc2
Kristiana Stefanova <christiana.stefanova@gmail.com>
2025-05-15 17:54:29 +0200

    Adding initials exercise

---
+++ 08.PetShop/Program.cs
@@ -0,0 +1,11 @@
+﻿
+double dogFood = double.Parse(Console.ReadLine()) * 2.50;
+double catFood = double.Parse(Console.ReadLine()) * 4;
+
+Console.WriteLine(dogFood + catFood + " " + "lv" + '.');
+
+
+
+
+
+
---
+++ 09.YardGreening/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double yardArea = double.Parse(Console.ReadLine());
+
+double priceForGreening = yardArea * 7.61;
+double discount = priceForGreening * 0.18;
+
+double finalPrice = priceForGreening - discount;
+
+Console.WriteLine($"The final price is: {finalPrice} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
