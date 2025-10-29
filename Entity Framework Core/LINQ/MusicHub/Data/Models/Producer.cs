aa0cab457983cc138cae93a5c1cada8ec72ee04d
kristiana95 <christiana.stefanova@gmail.com>
2025-10-29 18:15:23 +0100

    Implement LINQ queries for the 'MusicHub' project from the LINQ class exercises

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    using static Common.EntityValidations.Producer;
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public string? Pseudonym { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+            = new HashSet<Album>();  
+    } 
+}
---
+++ Entity Framework Core/LINQ/MusicHub/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MusicHub.ViewModels
+{
+    public class AlbumViewModel
+    {
+        public string Name { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string ProducerName { get; set; } = null!;
+
+        public decimal TotalPrice { get; set; }
+
+        public IEnumerable<SongViewModel> AlbumSongs { get; set; }
+            = new List<SongViewModel>();
+    }
+}
